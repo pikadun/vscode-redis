3f758676bd078267d271305eaf25af9f6bd82aee
wanggang <wg0121@outlook.com>
2020-04-02 01:29:46 +0800

    add some code

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,8 @@
 import { connect, Socket } from 'net'
 import { RedisCommand } from './constant';
 
+import RESP from './resp';
+
 
 interface Config {
     host: string;
@@ -10,17 +12,21 @@ interface Config {
 
 class Command {
     async run(cmd: RedisCommand, config: Config) {
-        const str = this.convert(cmd.split(' '));
         const socket = this.open(config.host, config.port);
+        const str = RESP.encode(cmd);
 
-        const result = new Promise((resolve) => {
+        const result = new Promise((resolve, reject) => {
             socket.on('data', (buffer) => {
-                resolve(this.dataHandle(buffer))
+                try {
+                    const result = RESP.decode({ buffer, offset: 0 })
+                    resolve(result)
+                } catch (error) {
+                    reject(error)
+                }
             })
         })
 
         socket.write(str);
-        socket.end();
         return result;
     }
 
@@ -31,23 +37,6 @@ class Command {
         socket.setNoDelay(true);
         return socket;
     }
-
-    private convert(args: string[]) {
-        let result = `*${args.length}\r\n`;
-
-        for (let i = 0; i < args.length; i++) {
-            const arg = args[i];
-            result += `$${Buffer.byteLength(arg)}\r\n${arg}\r\n`
-        }
-
-        return result;
-    }
-
-    private dataHandle(buffer: Buffer) {
-        const reply = buffer.toString();
-        return reply
-    }
-
 }
 
 export default new Command();
---
+++ src/resp.ts
@@ -0,0 +1,64 @@
+interface decodeObj {
+    buffer: Buffer,
+    offset: number
+}
+
+class RESP {
+    encode(cmd: string) {
+        const args = cmd.split(' ');
+        let result = `*${args.length}\r\n`;
+
+        for (let i = 0; i < args.length; i++) {
+            const arg = args[i];
+            result += `$${Buffer.byteLength(arg)}\r\n${arg}\r\n`
+        }
+        return result;
+    }
+
+    /**
+     * Handle redis reply
+     * @param buffer 
+     */
+    decode(obj: decodeObj): any {
+        const type = obj.buffer[obj.offset++];
+
+        switch (type) {
+            case 42: // '*' Arrays
+                return this.decodeArray(obj);
+            case 36: // '$' Bulk Strings
+                return this.decodeBulkString(obj)
+        }
+
+    }
+
+    private decodeArray(obj: decodeObj) {
+        const result = []
+        const count = this.length(obj);
+        obj.offset += count.toString().length;
+        for (let i = 0; i < count; i++) {
+            result.push(this.decode(obj))
+        }
+        return result;
+    }
+    private decodeBulkString(obj: decodeObj) {
+        const count = this.length(obj);
+        const u8a = new Uint8Array(count);
+
+        obj.offset += count.toString().length;
+        obj.buffer.copy(u8a, 0, obj.offset, obj.offset + count);
+
+        obj.offset += count + 2;
+        return Buffer.from(u8a).toString()
+    }
+
+    private length(obj: decodeObj) {
+        let len = 0;
+        let c = 0;
+        while ((c = obj.buffer[obj.offset++]) !== 13) {
+            len = (len * 10) + (c - 48);
+        }
+        return len;
+    }
+}
+
+export default new RESP()
