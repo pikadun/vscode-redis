538cb7f22647c031e66638c599c1169d25cc0a13
wanggang <wg0121@outlook.com>
2020-04-03 19:00:36 +0800

    adjust project

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { window, commands, ExtensionContext } from 'vscode';
-import RedisManager from './manager/manager';
+import Manager from './manager/manager';
 import AbstractNode from './node/abstraction';
 
 export function activate(context: ExtensionContext) {
-    const manager = new RedisManager(context);
-    window.registerTreeDataProvider('RedisManager', manager);
-    commands.registerCommand('RedisManager.AddConnection', () => manager.addConnection());
-    commands.registerCommand('RedisManager.Delete', (element: AbstractNode) => manager.delete(element));
+    const manager = new Manager(context);
+    const { Connection } = manager
+
+    window.registerTreeDataProvider('Connection', Connection);
+    commands.registerCommand('Connection.Add', () => Connection.add());
+    commands.registerCommand('Connection.Delete', (element: AbstractNode) => Connection.delete(element));
+    // commands.registerCommand('RedisManager.CreateTerminal', (element: AbstractNode) => manager.delete(element));
 }
---
+++ src/manager/terminal.ts
@@ -0,0 +1,9 @@
+import { ExtensionContext } from "vscode"
+
+class Terminal {
+    constructor(context: ExtensionContext) {
+        console.log(context)
+    }
+}
+
+export default Terminal
